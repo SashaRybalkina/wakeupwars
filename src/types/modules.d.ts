f670ba1af39fda28c009fe850a931b59ad92a4ce
Aleksandra Rybalkina <u1362376@utah.edu>
2025-11-19 23:09:59 +0000

    Edit modules.d.ts

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,3 +1,9 @@
+/**
+ * @file modules.d.ts
+ * @description TypeScript module declaration for importing SVG files in React Native.
+ */
+
+
 declare module '*.svg' {
   import React from 'react';
   import { SvgProps } from 'react-native-svg';
